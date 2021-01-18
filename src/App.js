7b2902c4caecec57c536e4f2b5bf7a53dc545a0e
Bryan Beshore <bryan.beshore@gmail.com>
2021-01-18 11:54:36 -0500

    Creating and submitting form data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, {Component} from 'react'
 import './App.css';
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
   state = {
-    characters: [
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-      },
-      {
-        name: 'Mac',
-        job: 'Bouncer',
-      },
-      {
-        name: 'Dee',
-        job: 'Aspiring Dancer',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ],
+    characters: [],
   }
 
   removeCharacter = (index) => {
@@ -35,21 +19,19 @@ class App extends Component {
   }
 
   render () {
-    const { characters } = this.state
+    const {characters} = this.state
 
     return (
       <div className="container">
         <Table characterData={characters} removeCharacter = {this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
       </div>
     )
   }
 
-  //   return (
-  //     <div className="container">
-  //       <Table characterData={characters} />
-  //     </div>
-  //   )
-  // }
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
+  }
 }
 
 export default App;
